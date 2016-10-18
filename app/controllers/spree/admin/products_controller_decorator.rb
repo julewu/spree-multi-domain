adb7658ea45186b40630eb17c13e8898d4a4c869
kun.zhang <kun.zhang@julewu.com>
2016-10-18 12:29:20 +0800

    修改store_ids  before update方法

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,20 +1,11 @@
 Spree::Admin::ProductsController.class_eval do
   update.before :set_stores
 
-  def update
-    store_ids = params[:product][:store_ids]
-    if params[:product].key? :store_ids
-      params[:product].delete :store_ids
-      store_ids.split(',').each do |store_id|
-        @object.products_stores.new(store_id: store_id)
-      end
-    end
-    super
-  end
-
   private
 
   def set_stores
-    @product.store_ids = nil unless params[:product].key? :store_ids
+    if params[:product][:store_ids].present?
+      params[:product][:store_ids] = params[:product][:store_ids].split(',')
+    end
   end
 end
