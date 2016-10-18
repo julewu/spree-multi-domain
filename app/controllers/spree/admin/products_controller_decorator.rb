a7b544eb2be8c18632dfae91c9529678d116d614
kun.zhang <kun.zhang@julewu.com>
2016-10-18 18:11:23 +0800

    修改store_ids校验规则

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -4,8 +4,16 @@ Spree::Admin::ProductsController.class_eval do
   private
 
   def set_stores
+    params[:product][:store_ids] = params[:product][:store_ids].split(',').map(&:to_i)
     if params[:product][:store_ids].present?
-      params[:product][:store_ids] = params[:product][:store_ids].split(',')
+      params[:product][:store_ids].each do |store_id|
+        @product.products_stores.new(store_id: store_id)
+      end
+      if @product.valid?
+        @product.products_stores.reload
+      else
+        params[:product].delete :store_ids
+      end
     end
   end
 end
