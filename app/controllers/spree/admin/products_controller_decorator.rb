21eb09df0096796e9e2e765ec523a1bd0a728e13
kun.zhang <kun.zhang@julewu.com>
2016-12-29 14:46:43 +0800

    修改params store_ids

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,17 +1,13 @@
 Spree::Admin::ProductsController.class_eval do
   update.before :set_stores
 
-  def update
-    store_ids = params[:product][:store_ids]
-    if store_ids.present?
-      params[:product][:store_ids] = store_ids.split(',')
-    end
-    super
-  end
-
   private
 
   def set_stores
-    @product.store_ids = nil unless params[:product].key? :store_ids
+    if params[:product].key? :store_ids
+      params[:product][:store_ids] = params[:product][:store_ids].split(',').map(&:to_i)
+    else
+      @product.store_ids = nil
+    end
   end
 end
